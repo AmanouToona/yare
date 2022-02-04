6af63ec28d7c0b70fe50175387260ca1e3fee0ad
toona <amanou.toona@gmail.com>
2022-02-05 00:41:26 +0900

    win medium-bot in 19m 26s.

--- beat_medium-bot.js
+++ beat_medium-bot.js
@@ -1,284 +1,333 @@
 function distance(a, b) {
-    return Math.sqrt((a.position[0] - b.position[0]) ** 2 + (a.position[1] - b.position[1]) ** 2)
+  return Math.sqrt(
+    (a.position[0] - b.position[0]) ** 2 + (a.position[1] - b.position[1]) ** 2
+  );
 }
 
 function beamable(a, b) {
-    d = distance(a, b)
-    return distance(a, b) < 190
+  d = distance(a, b);
+  return distance(a, b) < 199;
 }
 
 function beamable_enemy(s) {
-    if (s.sight.enemies.length === 0) {
-        return false
+  if (s.sight.enemies.length === 0) {
+    return false;
+  }
+  for (enemy of s.sight.enemies) {
+    if (beamable(s, spirits[enemy])) {
+      return spirits[enemy];
     }
-    for (enemy of s.sight.enemies) {
-        if (beamable(s, spirits[enemy])) {
-            return spirits[enemy]
-        }
-    }
-    return false
+  }
+  return false;
 }
 
-function beamable_friend(s, status) {
-    // status 中の友軍機へ供給
-    if (s.sight.friends.length === 0) {
-        return false
+function beam_enemy(s) {
+  if (s.sight.enemies.length == 0) return false;
+
+  for (enemy of s.sight.enemies) {
+    if (beamable(s, spirits[enemy])) {
+      s.energize(spirits[enemy]);
+      return true;
     }
-    for (friend of s.sight.friends) {
-        if (spirits[friend].mark != status) continue
-        if (spirits[friend].energy == spirits[friend].energy_capacity) continue
-        if (beamable(s, spirits[friend])) {
-            return spirits[friend]
-        }
+  }
+  return false;
+}
+
+function beam_friend(s, status) {
+  // status 中の友軍機へ供給
+  if (s.sight.friends.length === 0) {
+    return false;
+  }
+  for (friend of s.sight.friends) {
+    if (spirits[friend].mark != status) continue;
+    if (spirits[friend].energy == spirits[friend].energy_capacity) continue;
+    if (beamable(s, spirits[friend])) {
+      s.energize(spirits[friend]);
+      return true;
     }
-    return false
+  }
+  return false;
 }
 
 function stop_and_beam(from, to) {
-    if (!beamable(from, to)) {
-        if (from.structure_type == "star") {
-            to.move(from.position)
-        } else {
-            from.move(to.position)
-        }
+  if (!beamable(from, to)) {
+    if (from.structure_type == "star") {
+      to.move(from.position);
     } else {
-        if (to.structure_type == "star") return
-        if (from.structure_type == "star") to.energize(to)
-        else from.energize(to)
+      from.move(to.position);
     }
-    return
+  } else {
+    if (to.structure_type == "star") return;
+    if (from.structure_type == "star") to.energize(to);
+    else from.energize(to);
+  }
+  return;
 }
-
 
 function farming(s, from, to) {
-    s.shout("farmer")
-    if (s.energy == s.energy_capacity) {
-        s.set_mark("collected")
-    } else if (s.energy == 0) {
-        s.set_mark("collecting")
-    }
+  s.shout("farmer");
+  if (s.energy == s.energy_capacity) {
+    s.set_mark("collected");
+  } else if (s.energy == 0) {
+    s.set_mark("collecting");
+  }
 
-    if (s.mark == "collecting") {
-        stop_and_beam(from, s)
-        return
-    }
+  if (s.mark == "collecting") {
+    stop_and_beam(from, s);
+    return;
+  }
 
-    friend = beamable_friend(s, "attack")
-    if (friend != false) {
-        s.energize(friend)
-        return
-    }
+  if (beam_friend(s, "attack")) return;
+  if (beam_friend(s, "support")) return;
 
-    friend = beamable_friend(s, "support")
-    if (friend != false) {
-        s.energize(friend)
-        return
-    }
-
-    stop_and_beam(s, to)
-    return
+  stop_and_beam(s, to);
+  return;
 }
 
-function defend(s, base) {
-    s.set_mark("attack")
-    s.move(base.position)
+function defend_base(s) {
+  s.set_mark("attack");
+  s.shout("attack");
 
-    if (s.energy < s.energy_capacity / 2) {
-        s.energize(s)
-        return
-    }
-
-    enemy = beamable_enemy(s)
-
-    if (enemy != false) {
-        s.energize(enemy)
-        return
-    }
-    s.energize(enemy_base)
+  if (beam_enemy(s)) return;
+  s.move(base.position);
 }
 
-function fire(s) {
-    enemy = beamable_enemy(s)
-    if (enemy != false) {
-        s.energize(enemy)
-        return true
-    }
-    return false
+function defend_star(s, star) {
+  s.set_mark("attack");
+  s.shout("attack");
+
+  if (s.energy != 0 && beam_enemy(s)) return;
+  if (s.energy == s.energy_capacity) {
+    s.move(star.position);
+  } else {
+    stop_and_beam(star, s);
+  }
 }
 
-function scramble_attack(s, pos) {
-    s.shout("attack")
-    s.set_mark("attack")
+function defend_position(s, pos) {
+  s.set_mark("attack");
+  s.shout("attack");
 
-    attacking = fire(s)
-    if (attacking) return
-
-    // 余裕があるなら補給
-    if (typeof (pos) != Array && pos.structure_type == "star") {
-        stop_and_beam(pos, s)
-    }
-
-    s.move(pos)
-    return
+  if (beam_enemy(s)) return;
+  s.move(pos);
 }
 
-function print(a) {
-    console.log(a)
+function search(s) {
+  return s.sight.enemies.length > 0;
 }
 
-function scramble_support(s, pos, base = null) {
-    s.shout("support")
-    s.set_mark("support")
-
-    attacking = scramble_attack
-    if (attacking) return
-
-    // 戦闘中の友軍機には最優先供給
-    friend = beamable_friend(s, "attack")
-    if (s.energy != 0 && friend != false) {
-        s.energize(friend)
-        return
+function search_in_group(ss) {
+  for (s of ss) {
+    if (search(spirits[s])) {
+      return true;
     }
-
-    // 余裕があるなら補給
-    if (base != null && typeof (base) != Array && base.structure_type == "star") {
-        stop_and_beam(base, s)
-        return
-    }
-
-    s.move(pos)
-
+  }
+  return false;
 }
 
-function count_living_friends() {
-    count = 0
-    for (friend of my_spirits) {
-        if (friend.hp > 0) count++;
-    }
-    return count
+function make_line(s, pos1, pos2, i, tot) {
+  s.shout("attack");
+  if (tot == 0) return;
+  posx = pos1[0];
+  poxy = pos1[1];
+  if (tot != 1) {
+    x1 = pos1[0];
+    x2 = pos2[0];
+    y1 = pos1[1];
+    y2 = pos2[1];
+    posx = ((x1 - x2) / (tot - 1)) * i + x2;
+    posy = ((y1 - y2) / (tot - 1)) * i + y2;
+  }
+  if (beam_enemy(s)) return;
+  s.move([posx, posy]);
+  return;
+}
+
+function split_sortie(sortie, member, revers = false) {
+  if (revers) {
+    wait_sortie = sortie.slice(0, -member);
+    sortie = sortie.slice(-member);
+    return { sortie, wait_sortie };
+  }
+
+  wait_sortie = sortie.slice(member);
+  sortie = sortie.slice(0, member);
+  return {
+    sortie,
+    wait_sortie,
+  };
 }
 
 // setting
 if (base.position[0] == 1600) {
-    p_pos = 1
-    base_star = star_zxq
-    attack_pos1 = [1700, 800]
-    attack_pos2 = [1400, 1000]
-    support_pos1 = [1520, 800]
-    support_pos2 = [1210, 1000]
+  p_pos = 1;
+  base_star = star_zxq;
+  enemy_star = star_a1c;
+  base_front = [1600, 750];
+  base_star_front = [1100, 1000];
 } else {
-    p_pos = 2
-    base_star = star_a1c
-    attack_pos1 = [2500, 1600]
-    attack_pos2 = [2900, 1400]
-    support_pos1 = [2650, 1700]
-    support_pos2 = [2950, 1400]
+  p_pos = 2;
+  base_star = star_a1c;
+  enemy_star = star_zxq;
+  base_front = [2600, 1650];
+  base_star_front = [3100, 1400];
 }
-
-pos_s0 = base_star.position[0]
-pos_s1 = base_star.position[1]
-pos_b0 = base.position[0]
-pos_b1 = base.position[1]
-
-support_pos = [(pos_s0 + pos_b0) / 2, (pos_s1 + pos_b1) / 2]
-
-// counter
-tot = count_living_friends()
-console.log(tot)
 
 function main() {
-    live_splits = []
+  wait_sortie = [];
 
+  for (spirit of my_spirits) {
+    if (spirit.hp == 0) continue;
+    wait_sortie.push(spirit.id);
+  }
 
-}
+  console.log("friendly troops: ", wait_sortie.length);
 
-main()
+  if (wait_sortie.length >= 130) {
+    finish_blow = true;
+  } else if (wait_sortie.length < 100) {
+    finish_blow = false;
+  }
 
-
-i = -1
-for (id in my_spirits) {
-    s = my_spirits[id]
-
-    if (s.hp == 0) continue;
-    i++;
-
-    if (tot < 20) {
-        farming(s, base_star, base)
-        continue
+  if (finish_blow) {
+    for (id of wait_sortie) {
+      s = spirits[id];
+      s.energize(enemy_base);
+      defend_position(s, enemy_base.position);
     }
+    return;
+  }
 
-    if (false) {
-        // scrabmle
-        if (i < tot / 6 * 1) {
-            scramble_support(s, support_pos1)
-        } else if (i < tot / 6 * 2) {
-            scramble_support(s, support_pos2)
-        } else if (i < tot / 6 * 4) {
-            scramble_attack(s, attack_pos1)
-        } else {
-            scramble_attack(s, attack_pos2)
-        }
+  // 恒星エネルギー枯渇時は別の恒星からエネルギーを得る
+  if (base_star.energy <= 10) {
+    member = 20;
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+    for (id of sortie) {
+      s = spirits[id];
+      farming(s, star_p89, base);
     }
+  }
 
-    if (true) {
-        // scrabmle
-        if (i < tot / 3 * 1) {
-            scramble_support(s, support_pos1)
-        } else {
-            scramble_attack(s, attack_pos1)
-        }
+  if (wait_sortie.length >= 100) {
+    member = 20;
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+    for (id of sortie) {
+      s = spirits[id];
+      defend_star(s, enemy_star);
     }
+  }
 
-    if (false) {
-        // scrabmle
-        if (i < tot / 3 * 1) {
-            scramble_support(s, support_pos2)
-        } else {
-            scramble_attack(s, attack_pos2)
-        }
+  if (wait_sortie.length >= 30) {
+    member = 4;
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+    for (id of sortie) {
+      s = spirits[id];
+      defend_star(s, star_p89);
     }
+  }
 
-    if (tot < 100) {
-        d_member = tot / 3
-        if (i < d_member / 4 * 1) {
-            scramble_attack(s, attack_pos2)
-        } else if (i < d_member / 4 * 2) {
-            scramble_attack(s, attack_pos2)
-        } else if (i < d_member / 4 * 3) {
-            defend(s, base)
-        } else if (i < d_member) {
-            scramble_support(s, support_pos1)
-        } else {
-            farming(s, base_star, base, Math.floor(i / 2))
-        }
-        continue
+  if (wait_sortie.length >= 30) {
+    member = 4;
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+    for (id of sortie) {
+      s = spirits[id];
+      farming(s, star_p89, outpost);
     }
+  }
 
-    d_member = Math.min(tot / 4, 50)
-    if (i < d_member) {
-        if (i < d_member / 4 * 1) {
-            // scramble_support(s, support_pos1)
-            defend(s, star_zxq)
-        } else if (i < d_member / 4 * 2) {
-            scramble_support(s, support_pos1)
-        } else if (i < d_member / 4 * 3) {
-            // scramble_attack(s, attack_pos1)
-            defend(s, star_zxq)
-        } else {
-            scramble_attack(s, attack_pos2)
-        }
-    } else if (tot < 80) {
-        farming(s, base_star, base, Math.floor(i / 3))
-        continue
+  if (wait_sortie.length >= 40) {
+    member = 5;
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+    for (id of sortie) {
+      s = spirits[id];
+      farming(s, star_p89, outpost);
+    }
+  }
+
+  // 基地防衛
+  if (wait_sortie.length > 25) {
+    member = 4;
+    var { sortie, wait_sortie } = split_sortie(
+      wait_sortie,
+      member,
+      (revers = true)
+    );
+
+    for (id of sortie) {
+      s = spirits[id];
+      defend_base(s);
+    }
+  }
+
+  if (base.sight.enemies.length != 0) {
+    member = 6;
+    var { sortie, wait_sortie } = split_sortie(
+      wait_sortie,
+      member,
+      (revers = true)
+    );
+
+    for (id of sortie) {
+      s = spirits[id];
+      defend_base(s);
+    }
+  }
+
+  // 恒星防衛部隊
+  if (wait_sortie.length > 25) {
+    member = 5;
+    var { sortie, wait_sortie } = split_sortie(
+      wait_sortie,
+      member,
+      (revers = true)
+    );
+
+    for (id of sortie) {
+      s = spirits[id];
+      defend_star(s, base_star);
+    }
+  }
+
+  if (search_in_group(wait_sortie)) {
+    member = (wait_sortie.length / 3) * 2;
+    wait_sortie.slice(-10);
+    var { sortie, wait_sortie } = split_sortie(wait_sortie, member);
+
+    if (base.sight.enemies.length != 0) {
+      front_end = [
+        (base_front[0] + base_star_front[0]) / 2,
+        (base_front[1] + base_star_front[1]) / 2,
+      ];
     } else {
-        res = tot - d_member
-        if (i < 70) {
-            farming(s, base_star, base, Math.floor(i / 3))
-        } else if (i < d_member + res / 2) {
-            defend(s, star_p89)
-        } else {
-            defend(s, star_zxq)
-            farming(s, star_zxq, star_zxq)
-        }
+      front_end = base_star_front;
     }
+    for (i in sortie) {
+      id = sortie[i];
+      s = spirits[id];
+      make_line(s, base_front, base_star_front, i, sortie.length);
+      s.set_mark("attack");
+    }
+  }
+
+  if (wait_sortie.length > 25) {
+    member = 10;
+    var { sortie, wait_sortie } = split_sortie(
+      wait_sortie,
+      member,
+      (revers = true)
+    );
+
+    for (id of sortie) {
+      s = spirits[id];
+      defend_star(s, base_star);
+    }
+  }
+
+  // 補給
+  for (id of wait_sortie) {
+    s = spirits[id];
+    farming(s, base_star, base);
+  }
 }
+
+main();
